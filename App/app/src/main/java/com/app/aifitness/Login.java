65f1162949df4afac7deab73c4db762d72b2533b
Thang7804 <thang7804hq@.gmail.com>
2025-09-24 11:43:03 +0700

    First

---
+++ App/app/src/main/java/com/app/aifitness/Login.java
@@ -0,0 +1,88 @@
+package com.app.aifitness;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class Login extends AppCompatActivity {
+
+    private EditText edtEmail, edtPassword;
+    private Button btnLogin;
+    private TextView txSignUp;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        edtEmail = findViewById(R.id.Email);
+        edtPassword = findViewById(R.id.Password);
+        btnLogin = findViewById(R.id.btnSignIn);
+        txSignUp = findViewById(R.id.txSignin);
+        mAuth = FirebaseAuth.getInstance();
+        btnLogin.setOnClickListener(v -> loginUser());
+        txSignUp.setOnClickListener(v -> {
+            startActivity(new Intent(Login.this, SignUp.class));
+        });
+    }
+
+    private void loginUser() {
+        String email = edtEmail.getText().toString().trim();
+        String password = edtPassword.getText().toString().trim();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                        if (firebaseUser == null) {
+                            Toast.makeText(Login.this, "User is null", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        String uid = firebaseUser.getUid();
+                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                        db.collection("users").document(uid)
+                                .get()
+                                .addOnSuccessListener(documentSnapshot -> {
+                                    if (documentSnapshot.exists()) {
+                                        Boolean isNew = documentSnapshot.getBoolean("isNew");
+                                        runOnUiThread(() -> {
+                                            if (isNew != null && isNew) {
+                                                startActivity(new Intent(Login.this, Question.class));
+                                            } else {
+                                                startActivity(new Intent(Login.this, MainActivity.class));
+                                            }
+                                            finish();
+                                        });
+                                    }
+                                })
+                                .addOnFailureListener(e ->
+                                        Toast.makeText(Login.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show()
+                                );
+
+                    } else {
+                        Toast.makeText(Login.this, "Fail: " +
+                                task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    }
